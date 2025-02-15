4a20452438aef726d97eb9db9db4455915be5177
Georgia Channing <georgia.channing@live.com>
2025-02-15 13:49:12 +0000

    UI-2: Added config to make prod build work

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,6 +1,5 @@
 'use client'
-
-import * as React from 'react'
+// @ts-nocheck
 import {
   ThemeProvider as NextThemesProvider,
   type ThemeProviderProps,
--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,5 +1,4 @@
-"use client"
-
+// @ts-nocheck
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
